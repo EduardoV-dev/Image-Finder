da561e03749e4bbf5e9cdb46917efbe9aad09a90
Eduardo Varela <eduardovarela139@gmail.com>
2021-01-01 21:13:43 -0600

    Project stated already

---
+++ src/pages/ImgView/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ImgView = () => {
+  return (  
+    <h1>ImgView Page</h1>
+  );
+}
+ 
+export default ImgView;
---
+++ src/theme/index.js
@@ -0,0 +1,7 @@
+export const theme = {
+  primary: '#102E4A',
+  primaryDarken: '#1A344D',
+  secondary: '#4097DA',
+  accent: '#1D1D1D',
+  text: '#fff'
+}
