ed0f4e3f0e7b45d416a07e1193d779970a16be8d
Eduardo Varela <eduardovarela139@gmail.com>
2022-07-23 16:48:11 -0600

    App done

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    rootDir: ['./src'],
-    moduleDirectories: ['node_modules', 'src'],
-    moduleFileExtensions: ['js', 'jsx'],
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-    testEnvironment: 'jest-environment-jsdom',
-    modulePaths: ['<rootDir>/src'],
-};
